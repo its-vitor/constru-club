ef35d870e47b0221d5e71f0ed5852e901dff7b24
vitor.dev <146399948+its-vitor@users.noreply.github.com>
2023-10-30 21:34:03 -0300

    Update register.js

--- Website/js/login-register/modules/register.js
+++ Website/js/login-register/modules/register.js
@@ -1,7 +1,7 @@
 import ValidarCpf from "./validate-cpf.js";
 
 export default function register() {
-  const url = "construclub.squareweb.app";
+  const url = "https://construclub.squareweb.app";
   const form = document.querySelectorAll("form")[1];
   const erro = document.querySelector("#register-error");
   const { name, email, cpf, password } = form;
